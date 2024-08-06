e0c329d9a4accdfb510c8205e5a46d99b4b60f67
Thomas Constantine Moore <tomismore@gmail.com>
2024-08-06 15:33:00 -0400

    passing tests in react lib

--- packages/samples/examples/index.ts
+++ packages/samples/examples/index.ts
@@ -4,11 +4,13 @@ import { Merchant, Receipt } from "versa_unstable_sdk";
 import rocketco from "./senders/rocketco.json";
 import bend from "./senders/bend.json";
 
+import untypedFlight from "./receipts/flight.json";
 import untypedRideshare from "./receipts/rideshare_polyline.json";
 import untypedSimple from "./receipts/simple.json";
 import untypedSubscription from "./receipts/subscription.json";
 import untypedRail from "./receipts/rail.json";
 
+const flight = untypedFlight as Receipt;
 const rail = untypedRail as Receipt;
 const rideshare = untypedRideshare as Receipt;
 const simple = untypedSimple as Receipt;
@@ -19,6 +21,7 @@ export const senders: Record<string, Merchant> = {
   bend,
 };
 export const receipts: Record<string, Receipt> = {
+  flight,
   rideshare,
   simple,
   subscription,
