3efa65ae97841eb3d2e17e7a8056c1cf4dcfb893
Thomas Constantine Moore <tomismore@gmail.com>
2024-10-23 15:56:57 -0400

    rename 'home' app to 'demo'

--- apps/demo/src/app/page.tsx
+++ apps/demo/src/app/page.tsx
@@ -1,9 +1,17 @@
 import styles from "./page.module.css";
 import InteractiveStudio from "@/components/studio/interactiveStudio";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image
+        src="https://versa.org/versa-logo.svg"
+        width={96}
+        height={25}
+        alt=""
+        className={styles.logo}
+      />
       <InteractiveStudio />
     </main>
   );
