38188b5b73c460699f246166be9e22a8096c4916
Thomas Constantine Moore <thomas@versa.org>
2024-11-07 16:32:23 -0500

    better rollup release build

---
+++ packages/react/release.config.ts
@@ -0,0 +1,41 @@
+// rollup.config.js
+
+import { bundleStats } from "rollup-plugin-bundle-stats";
+import commonjs from "@rollup/plugin-commonjs";
+import del from "rollup-plugin-delete";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { RollupOptions } from "rollup";
+
+const config: RollupOptions = {
+  input: "src/index.ts",
+  output: [
+    {
+      dir: "dist",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      dir: "dist",
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    del({ targets: "dist/*", runOnce: true }),
+    (peerDepsExternal as any)(),
+    resolve(),
+    typescript(),
+    commonjs(),
+    postcss({
+      modules: true,
+    }),
+    bundleStats(),
+  ],
+  external: ["react"],
+};
+
+export default config;
